3f39f8b0e1f5261b9f9870c19e31f87f60837f64
ptabasso2 <pejman.tabassomi@gmail.com>
2025-01-02 13:24:01 +0000

    Allowing @CrossOrigin requests in spring-backend

--- spring-backend/src/main/java/com/datadoghq/pej/springbackend/controller/ApiController.java
+++ spring-backend/src/main/java/com/datadoghq/pej/springbackend/controller/ApiController.java
@@ -2,10 +2,12 @@ package com.datadoghq.pej.springbackend.controller;
 
 
 import com.datadoghq.pej.springbackend.model.ApiResponse;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class ApiController {
